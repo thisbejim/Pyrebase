adf20f2e7b7979e2361324ab318ee0a9a6330e9b
thisbejim <jimmy_cm3@hotmail.com>
2015-12-17 11:34:02 +1100

    v

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Pyrebase',
-    version='0.3.43',
+    version='0.3.44',
     url='https://github.com/thisbejim/Pyrebase',
     description='A simple python wrapper for the Firebase API',
     author='https://github.com/thisbejim/Pyrebase/graphs/contributors',
