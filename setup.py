921a2126889186a65fbf81c7ee91a6178846619b
James Childs-Maidment <jimmy_cm3@hotmail.com>
2016-05-20 15:10:46 +1000

    2.0 to match firebase versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Pyrebase',
-    version='0.3.49',
+    version='2.0.0',
     url='https://github.com/thisbejim/Pyrebase',
     description='A simple python wrapper for the Firebase API',
     author='James Childs-Maidment',
