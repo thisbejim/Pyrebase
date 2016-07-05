529f119ccd0bc84bca9ca4768ebd8234cea74143
James Childs-Maidment <jimmy_cm3@hotmail.com>
2016-07-05 18:10:51 +1000

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Pyrebase',
-    version='3.0.4',
+    version='3.0.5',
     url='https://github.com/thisbejim/Pyrebase',
     description='A simple python wrapper for the Firebase API',
     author='James Childs-Maidment',
