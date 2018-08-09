f3bf465611117eec7e01678d10035da2db077f78
nhorvath <nhorvath@gmail.com>
2018-08-08 20:39:05 -0400

    add instructions for test config

--- tests/config.template.py
+++ tests/config.template.py
@@ -1,3 +1,5 @@
+# get this from firebase console
+# go to project settings, general tab and click "Add Firebase to your web app"
 SIMPLE_CONFIG = {
     "apiKey": "",
     "authDomain": "",
@@ -5,6 +7,8 @@ SIMPLE_CONFIG = {
     "storageBucket": "",
 }
 
+# get this json file from firebase console
+# go to project settings, service accounts tab and click generate new private key
 SERVICE_ACCOUNT_PATH = "secret.json"
 
 SERVICE_CONFIG = dict(SIMPLE_CONFIG, serviceAccount=SERVICE_ACCOUNT_PATH)
