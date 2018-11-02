c744bbd139e5c25448282c1b7fb1f6f887f81613
digitake <digitake@users.noreply.github.com>
2018-11-02 14:04:54 +0700

    Update setup.py package info and dependency
    - Change some of Info as we gonna forked out the repo but not gonna change the package name.

--- setup.py
+++ setup.py
@@ -2,25 +2,25 @@ from setuptools import setup, find_packages
 
 setup(
     name='Pyrebase',
-    version='3.0.27',
-    url='https://github.com/thisbejim/Pyrebase',
-    description='A simple python wrapper for the Firebase API',
-    author='James Childs-Maidment',
+    version='3.1.0',
+    url='https://github.com/digitake/Pyrebase',
+    description='A forked of thisbejim of a simple python wrapper for the Firebase API',
+    author='Digitake',
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='Firebase',
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'requests==2.11.1',
-        'gcloud==0.17.0',
-        'oauth2client==3.0.0',
-        'requests_toolbelt==0.7.0',
-        'python_jwt==2.0.1',
-        'pycryptodome==3.4.3'
+        'requests==2.20.9',
+        'gcloud==0.18.3',
+        'oauth2client==4.1.3',
+        'requests_toolbelt==0.8.0',
+        'python_jwt==3.2.3',
+        'pycryptodome==3.7.0'
     ]
 )
