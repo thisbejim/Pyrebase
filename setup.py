73d133a86a2aa9fea25fda63aae19e4942c0d37d
Brian Lee <briandl92391@gmail.com>
2021-05-03 12:00:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,11 +16,11 @@ setup(
     keywords='Firebase',
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'requests==2.11.1',
-        'gcloud==0.17.0',
-        'oauth2client==3.0.0',
-        'requests_toolbelt==0.7.0',
-        'python_jwt==2.0.1',
-        'pycryptodome==3.4.3'
+        'requests',
+        'gcloud',
+        'oauth2client',
+        'requests_toolbelt',
+        'python_jwt',
+        'pycryptodome'
     ]
 )
